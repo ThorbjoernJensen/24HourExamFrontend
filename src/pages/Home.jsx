3ec8900e2b9ee4d6d3cadb9dcfd48ccfae51d812
Thorbjørn <cph-tj308@cphbusiness.dk>
2023-01-19 09:07:51 +0100

    renaming

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home(props) {
+  return (
+    <div>
+      <h3>Welcome to the Conference System</h3>
+        <p>24 hour exam - frontend - SP8  </p>
+    </div>
+  );
+}
+
+export default Home;
